d645e35c3d1e87c17b9f70e0a4b25ad35e79e2ac
Marina Matulle <marinamatulle.mm@gmail.com>
2025-08-11 23:22:49 -0300

    fix: corrige problemas com nomes errados de date para startDate na migration e corrige erro de enum no eventSchemas

--- src/schemas/eventSchemas.ts
+++ src/schemas/eventSchemas.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const eventSchema = z.object({
   type: z.string().min(1),
   value: z.number().positive(),
-  frequency: z.enum(["única", "mensal", "anual"]),
+  frequency: z.enum(["ONCE", "MONTHLY", "ANNUAL"]),
   startDate: z.string().datetime(),
 });
 
