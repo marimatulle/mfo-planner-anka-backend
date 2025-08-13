4e7d4785297f1a2736af114a860c7c5ef945636e
Marina Matulle <marinamatulle.mm@gmail.com>
2025-08-13 16:35:41 -0300

    chore: modificações para rodar o server e add mensagem de debug

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import swagger from "@fastify/swagger";
 import swaggerUI from "@fastify/swagger-ui";
 import jwt from "@fastify/jwt";
 import dotenv from "dotenv";
+import chalk from "chalk";
 import { registerRoutes } from "./routes/routes";
 
 dotenv.config();
@@ -34,8 +35,19 @@ app.get("/", async () => ({ hello: "world" }));
 (async () => {
   await registerRoutes(app);
 
-  app.listen({ port: 3000, host: "0.0.0.0" }).then(() => {
-    console.log("Servidor rodando em http://localhost:3000");
-    console.log("Docs em http://localhost:3000/docs");
+  console.log(chalk.cyan("\n=== Rotas registradas ===\n"));
+  app
+    .printRoutes()
+    .split("\n")
+    .forEach((line) => {
+      if (line.includes("POST")) console.log(chalk.green(line));
+      else if (line.includes("GET")) console.log(chalk.blue(line));
+      else console.log(line);
+    });
+  console.log(chalk.cyan("\n========================\n"));
+
+  app.listen({ port: 3001, host: "0.0.0.0" }).then(() => {
+    console.log(chalk.yellow("Servidor rodando em http://localhost:3001"));
+    console.log(chalk.yellow("Docs em http://localhost:3001/docs"));
   });
 })();
