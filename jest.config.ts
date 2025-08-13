114c92cc2ae75a8dbe2fbf103b544722b246b2d6
Marina Matulle <marinamatulle.mm@gmail.com>
2025-08-13 16:55:09 -0300

    add configs do jest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  testMatch: ["<rootDir>/src/test/**/*.test.ts"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
---
+++ src/test/example.test.ts
@@ -0,0 +1,6 @@
+describe("Exemplo de teste", () => {
+  it("deve somar corretamente", () => {
+    const resultado = 2 + 2;
+    expect(resultado).toBe(4);
+  });
+});
